d7ec88df21d83a9cfcc4c0480b6b824207347e6f
MaratKabylov <mskabylov@gmail.com>
2026-09-15 10:23:59 +0500

    Пересборка

--- apps/api/src/modules/outbox/outbox.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OutboxService } from './outbox.service';
-import { PrismaService } from '../../common/prisma/prisma.service';
-
-@Module({
-  providers: [OutboxService, PrismaService],
-  exports: [OutboxService],
-})
-export class OutboxModule {}
--- infrastructure/docker/init-extensions.sql
+++
@@ -1,3 +0,0 @@
--- Enable extensions required by Dental SaaS architecture
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pg_trgm";
